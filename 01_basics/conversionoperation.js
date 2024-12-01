d2283bf740e63c4762864592901ac89cc25cdf70
Aradhya Singh <aradhyasinghhh231@gmail.com>
2024-12-01 13:47:18 +0000

    config done

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,32 @@
+let score = 33
+
+let valueIn = Number(score)
+console.log(typeof valueIn);
+
+let isloggedIn = 1
+let booleanIn = Boolean(isloggedIn)
+console.log(booleanIn);
+
+// OPERATIONS
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //it means 2 raised to power 2
+// console.log(2/2);
+// console.log(2%2);
+//all above are basic operations
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
+
+// let str1 = "Aradhya"
+// let str2 = " Singh"
+// console.log("str1" + "str2");
+
+let num1,num2,num3
+num1=num2=num3= 2+2 
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
+let age=18
+let isloggedIn=true
+
+//number => 2 to power 53
+//bigInt
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => uniqueness
+
+//object
+console.log(typeof age)
